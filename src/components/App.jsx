d4980f99b2984708016865ab4a4d8e5dc82c9d9c
Oleksy Lykov <lukov_jr@ukr.net>
2023-05-30 15:27:34 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+import Home from "pages/Home";
+import Movies from "pages/Movies";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template HELLO
+    <div>
+      <nav>
+        <NavLink to="/" end>Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/pages/Movies.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Movies = () => { 
+    
+    return <div>Movie detail</div>;
+};
+
+export default Movies;
